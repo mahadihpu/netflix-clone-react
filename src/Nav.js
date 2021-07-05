cfa6138373cd161406011085741d413821a17e2a
mahadihpu <46655666+mahadihpu@users.noreply.github.com>
2021-07-05 23:03:25 +0600

    Added most of the files.The website looks professinal now. Maybe needs some more work

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
 
 const Nav = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="netflix logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt="netflix logo"
+      />
+    </div>
+  );
 };
 
 export default Nav;
